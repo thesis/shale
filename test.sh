1d5b5db2c0344d5ab9785015724abbc3748e7745
Chris Martin <ch.martin@gmail.com>
2014-09-18 19:35:36 -0400

    fix the testing structure

--- test.sh
+++ test.sh
@@ -1,9 +1,29 @@
-#/usr/bin/env bash
+#!/bin/bash
+
+cleanup() {
+    kill $(jobs -pr)
+
+    # Workaround for bug in Flask in Python 2
+    # https://github.com/mitsuhiko/flask/issues/988
+    kill $(fuser -n tcp 5000 2> /dev/null)
+}
+
+trap "cleanup" EXIT
+
+PATH="$PATH:$PWD/phantomjs/bin/"
+
+/bin/java -jar selenium -role hub &
+
+sleep 1
+
+/bin/java -jar selenium -role node \
+  -hub http://localhost:4444/register/grid \
+  -nodeConfig ./libs/selenium/nodeConfig.json &
 
 python wsgi.py &
+
 sleep 2
-SERVER_PID=$!
 nosetests shale
 STATUS=$?
-kill $SERVER_PID
+
 exit $STATUS
