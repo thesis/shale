80d62f3dcc3c7d30963053cfdb9ef3cddfb0f365
Matt Luongo <mhluongo@gmail.com>
2014-09-25 22:30:39 -0400

    Begin moving the backend to clojure.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject shale "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler shale.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
