99a17fc9ba75c7b2a340800ac30f3773abf84f4d
Matt Luongo <mhluongo@gmail.com>
2014-09-29 17:28:29 -0400

    Include the servlet API for deploys.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.1"]
+                 [javax.servlet/servlet-api "2.5"]
                  [compojure "1.1.9"]
                  [liberator "0.12.2"]
                  [clj-json "0.5.3"]
@@ -14,11 +15,10 @@
                  [com.brweber2/clj-dns "0.0.2"]
                  [org.bovinegenius/exploding-fish "0.3.4"]]
   :auto-clean false
-  :uberjar {:aot :all}
   :main shale.handler
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler shale.handler/app
          :port 5000}
   :profiles {:dev
-             {:dependencies [[javax.servlet/servlet-api "2.5"]
-                             [org.clojure/tools.trace "0.7.8"]]}})
+             {:dependencies [[org.clojure/tools.trace "0.7.8"]]}
+             :uberjar {:aot :all}})
