635a7820364b99d215ff9b46daeae632bb3e7f01
Matt Luongo <mhluongo@gmail.com>
2014-09-29 22:35:04 -0400

    Fleshed out the DefaultNodePool implementation.

--- src/shale/node-pool.clj
+++ src/shale/node-pool.clj
@@ -1,5 +1,3 @@
-
-
 (defprotocol NodePool
   "Basic interface for choosing and managing Selenium nodes per session.
    Implementing this allows dynamic node domains- eg, by retrieving them from
@@ -9,7 +7,16 @@
   (add-node [this requirements] "")
   (remove-node [this url] ""))
 
-(deftype DefaultNodePool [options] NodePool
-  ""
-  ()
+(deftype DefaultNodePool [nodes] NodePool
+  "A simple node pool the chooses randomly from an initial list."
+  (get-node [this requirements]
+    (rand-nth nodes))
+
+  (add-node [this requirements]
+    (throw (ex-info "Unable to add new nodes to the default node pool."
+                    {:user-visible true :status 500})))
+
+  (remove-node [this requirements]
+    (throw (ex-info "Unable to remove nodes with the default node pool."
+                    {:user-visible true :status 500})))
   )
