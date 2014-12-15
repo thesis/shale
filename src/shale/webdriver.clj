25daad9aadb37d34482c76aa0f4ee57f4d93828b
Matt Luongo <mhluongo@gmail.com>
2014-12-14 20:47:12 -0500

    Fixed a bug executing JS from the clojure client.

--- src/shale/webdriver.clj
+++ src/shale/webdriver.clj
@@ -10,34 +10,37 @@
            org.openqa.selenium.Capabilities
            clj_webdriver.ext.remote.RemoteWebDriverExt
            java.net.URL)
-  (:use [clj-webdriver.remote.driver :only[session-id]]
+  (:use [clj-webdriver.remote.driver :only [session-id]]
         clj-webdriver.taxi))
 
 (defn new-webdriver [node capabilities]
-  (def wd
-    (new clj_webdriver.ext.remote.RemoteWebDriverExt
-      (new URL node) (new DesiredCapabilities capabilities)))
-  (clj-webdriver.driver/init-driver wd))
+  (clj-webdriver.driver/init-driver
+    (RemoteWebDriverExt. (URL. node)
+                         (DesiredCapabilities. capabilities))))
 
 (defn resume-webdriver [session-id node capabilities]
   ;; this is pretty dirty- refactor with a protocol?
   (doall
     (map #(.setAccessible % true)
          (.getDeclaredConstructors RemoteWebDriverExt)))
-  (def wd (RemoteWebDriverExt.))
-  (wall.hack/method RemoteWebDriverExt
-                    :setCommandExecutor
-                    [CommandExecutor]
-                    wd
-                    (HttpCommandExecutor. (URL. node)))
-  (wall.hack/method RemoteWebDriver
-                    :init
-                    [Capabilities Capabilities]
-                    wd
-                    (DesiredCapabilities. capabilities)
-                    nil)
-  (wall.hack/method (.getClass wd) :setSessionId  [String] wd session-id)
-  (clj-webdriver.driver/init-driver wd))
+  (let [wd (RemoteWebDriverExt.)
+        capabilities (DesiredCapabilities. capabilities)
+        cap-field (.getDeclaredField RemoteWebDriver "capabilities")]
+    (wall.hack/method RemoteWebDriverExt
+                      :setCommandExecutor
+                      [CommandExecutor]
+                      wd
+                      (HttpCommandExecutor. (URL. node)))
+    (wall.hack/method RemoteWebDriver
+                      :init
+                      [Capabilities Capabilities]
+                      wd
+                      capabilities
+                      nil)
+    (wall.hack/method (.getClass wd) :setSessionId [String] wd session-id)
+    (.setAccessible cap-field true)
+    (.set cap-field wd capabilities)
+    (clj-webdriver.driver/init-driver wd)))
 
 (defn to-async [wd url]
   (def js "setTimeout(function(){window.location=\"%s\";}, 10);")
