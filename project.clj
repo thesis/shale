e4fc3a6e1f81bc58c32f6b7e66eff473ad2aa519
Matt Luongo <mhluongo@gmail.com>
2016-05-17 20:52:00 -0400

    lein-release plugin: preparing 0.2.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shale "0.2.1-SNAPSHOT"
+(defproject shale "0.2.1"
   :description "A Clojure-backed Selenium hub replacement"
   :url "https://github.com/cardforcoin/shale"
   :dependencies [[org.clojure/clojure "1.7.0"]
