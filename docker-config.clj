16270209a0a48e57880ebbb9d0cbf4622be8fb46
Matt Luongo <mhluongo@gmail.com>
2016-07-12 20:54:43 -0400

    Including building the uberjar in the Dockerfile.

---
+++ docker-config.clj
@@ -0,0 +1,8 @@
+{:node-list ["http://selenium-1:4444/wd/hub" "http://selenium-2:4444/wd/hub"]
+ :node-max-sessions 6
+ :port 5000
+ :webdriver-timeout 3000
+ :start-webdriver-timeout 5000
+  :redis {:host "redis"
+         :port 6379
+         :db 0}}
