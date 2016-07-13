b7b217bd6ea878c736812b05ca2e0dba037a4700
Matt Luongo <mhluongo@gmail.com>
2016-07-12 22:58:30 -0700

    Increase test timeouts.

--- test-config.clj
+++ test-config.clj
@@ -1,3 +1,5 @@
 {:node-list ["http://localhost:4444/wd/hub" "http://localhost:4443/wd/hub"]
  :node-max-sessions 6
- :port 5000}
+ :port 5000
+ :webdriver-timeout 3000
+ :start-webdriver-timeout 5000}
