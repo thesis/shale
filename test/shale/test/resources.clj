be35e73d19e07fa2c2f2135a3aeb3e9b9c41ccd1
Matt Luongo <mhluongo@gmail.com>
2016-07-29 21:28:57 -0700

    Remove tests for ->session-request
    It's beem removed.

--- test/shale/test/resources.clj
+++ test/shale/test/resources.clj
@@ -1,6 +1,6 @@
 (ns shale.test.resources
   (:require [clojure.test :refer :all]
-            [shale.resources :refer [parse-request-data ->sessions-request]]
+            [shale.resources :refer [parse-request-data]]
             [schema.core :as s]))
 
 (deftest test-parse-request-data
@@ -49,27 +49,3 @@
         (is (=
           (parse-request-data :context context)
           [false {:shale.resources/data {"a" 1}}]))))))
-
-(deftest test-->sessions-resource
-
-  (testing "->sessions-request"
-
-    (testing "1"
-      (let [context {:shale.resources/data {"browser_name" "phantomjs"}}
-            sessions-request {:browser-name "phantomjs"}]
-        (is (->sessions-request context) sessions-request)))
-
-    (testing "2"
-      (let [context {:shale.resources/data {"browser_name" "phantomjs"
-                                            "tags" ["walmart"]}}
-            sessions-request {:browser-name "phantomjs"
-                              :tags ["walmart"]}]
-        (is (->sessions-request context) sessions-request)))
-
-    (testing "3"
-      (let [context {:shale.resources/data {"tags" ["walmart" "logged-in"]
-                                            "reserved" false}}
-            sessions-request {:tags ["walmart" "logged-in"]
-                              :reserved false}]
-        (is (->sessions-request context) sessions-request)))
-))
