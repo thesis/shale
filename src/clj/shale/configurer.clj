8bbe59ce26a0560bf801ab92fbdbabff17288a3b
Matt Luongo <mhluongo@gmail.com>
2017-03-28 13:26:35 -0700

    Optionally load config values from env vars.

--- src/clj/shale/configurer.clj
+++ src/clj/shale/configurer.clj
@@ -2,10 +2,22 @@
     (:use shale.utils)
     (:require [carica.core :refer [resources]]
               [environ.core :refer [env]]
+              clojure.walk
               [clojure.java.io :as io]
               clojure.edn
               [shale.logging :as logging]
               [shale.utils :refer [pretty]]))
+
+
+(defn strip-namespace [kw]
+  (keyword (name kw)))
+
+(defn resolve-env-keyword!
+  "If a keyword has the namespace :env, look it up in the environment and return the env var value"
+  [kw]
+  (if (and (keyword? kw) (namespace kw) (= "env" (namespace kw)))
+    (get env (strip-namespace kw))
+    kw))
 
 (defn get-config
   "Get the config file. Path can either be specified by a CONFIG_FILE
@@ -18,7 +30,8 @@
                           (filter identity)
                           (map io/as-url)
                           concat)
-        config (clojure.edn/read-string (slurp (first config-paths)))]
-    (logging/info "Loaded shale config...\n")
-    (logging/info (pretty config))
-    config))
+        raw-config (clojure.edn/read-string (slurp (first config-paths)))]
+    (logging/info "Loading config...\n")
+    (logging/info (pretty raw-config))
+    (logging/info "Resolving environment variables...\n")
+    (clojure.walk/postwalk resolve-env-keyword! raw-config)))
