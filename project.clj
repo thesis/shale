321889ae6d7e2cf0cb331671a811e3af465cb4ce
Allen Rohner <arohner@gmail.com>
2017-04-11 12:57:14 -0500

    Version 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shale "0.3.3-SNAPSHOT"
+(defproject shale "0.3.3"
   :description "A Clojure-backed Selenium hub replacement"
   :url "https://github.com/cardforcoin/shale"
   :license {:name "MIT License"
